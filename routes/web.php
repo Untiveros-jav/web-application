d6901292d708e11c986bcd4d3703e2286a007043
Untiveros-jav <untiveros2024@gmail.com>
2025-05-28 01:01:07 -0500

    Subiendo los demas archivos

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmpleadoController;
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+/*
+Route::get('/empleado', function () {
+    return view('empleado.index');
+});
+
+Route::get('/empleado/create',[EmpleadoController::class,'create']);
+*/
+Route::resource('empleado', EmpleadoController::class)->middleware('auth');
+Auth::routes(['register'=>false,'reset'=>false]);
+
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/home', [EmpleadoController::class, 'index'])->name('home');
+
+Route::group(['middleware'=>'auth'], function(){
+
+    Route::get('/', [EmpleadoController::class, 'index'])->name('home');
+
+});
